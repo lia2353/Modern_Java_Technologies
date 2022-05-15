925aa5c4a8828a65e504be748adf02386e35ec7f
lia2353 <yana97@abv.bg>
2022-05-15 21:00:26 +0300

    Add files via upload

---
+++ Lab1/02-sandwich/SandwichExtractor_mySolution.java
@@ -0,0 +1,27 @@
+package sandwich;
+
+import java.util.Arrays;
+
+public class SandwichExtractor_mySolution {
+    public static String[] extractIngredients(String sandwich) {
+        String[] splitSandwich = sandwich.split("bread");
+
+        if (splitSandwich.length != 3) {
+            return new String[0];
+        }
+
+        String[] ingredients = splitSandwich[1].split("-");
+
+        int newSize = 0;
+        for (int i = 0; i < ingredients.length; ++i) {
+            if (!ingredients[i].equals("olives")) {
+                ingredients[newSize++] = ingredients[i];
+            }
+        }
+
+        String[] extractedIngredients = Arrays.copyOfRange(ingredients, 0, newSize);
+        Arrays.sort(extractedIngredients);
+
+        return extractedIngredients;
+    }
+}
