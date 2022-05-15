4fa534344440d45b0cfa8ff6bf35e7440e48e095
lia2353 <yana97@abv.bg>
2022-05-15 23:10:35 +0300

    Add files via upload

---
+++ Lab1/ArrayAnalyser.java
@@ -0,0 +1,21 @@
+public class ArrayAnalyser {
+    public static boolean isMountainArray(int[] array) {
+        if (array.length < 3) {
+            return false;
+        }
+
+        // Climb Up
+        int position = 0;
+        int length = array.length;
+        while (((position + 1) < length) && (array[position] < array[position + 1])) {
+            ++position;
+        }
+
+        // Climb Down
+        while (((position + 1) < length) && (array[position] > array[position + 1])) {
+            ++position;
+        }
+
+        return position == length - 1;
+    }
+}
