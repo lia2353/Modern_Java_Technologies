76cccfc4c9abc6c1560cf05680d8935b3d83f252
lia2353 <yana97@abv.bg>
2022-05-28 13:27:41 +0300

    Update ContentNotFoundException.java

--- Lab3/Netflix/src/bg/sofia/uni/fmi/mjt/netflix/exceptions/ContentNotFoundException.java
+++ Lab3/Netflix/src/bg/sofia/uni/fmi/mjt/netflix/exceptions/ContentNotFoundException.java
@@ -1,6 +1,6 @@
 package bg.sofia.uni.fmi.mjt.netflix.exceptions;
 
-//The RuntimeException class is the superclass of all unchecked exceptions
+// The RuntimeException class is the superclass of all unchecked exceptions
 public class ContentNotFoundException extends RuntimeException {
     public ContentNotFoundException(String message) {
         super(message);
