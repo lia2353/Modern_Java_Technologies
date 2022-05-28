6c780f542824857598f961b6121f54ad35886ee3
lia2353 <yana97@abv.bg>
2022-05-28 13:28:26 +0300

    Update UserNotFoundException.java

--- Lab3/Netflix/src/bg/sofia/uni/fmi/mjt/netflix/exceptions/UserNotFoundException.java
+++ Lab3/Netflix/src/bg/sofia/uni/fmi/mjt/netflix/exceptions/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package bg.sofia.uni.fmi.mjt.netflix.exceptions;
 
-//The RuntimeException class is the superclass of all unchecked exceptions
+// The RuntimeException class is the superclass of all unchecked exceptions
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super(message);
