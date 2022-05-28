1abd77e914f5649422aa633eabb531caf509a26d
lia2353 <yana97@abv.bg>
2022-05-28 13:28:07 +0300

    Update ContentUnavailableException.java

--- Lab3/Netflix/src/bg/sofia/uni/fmi/mjt/netflix/exceptions/ContentUnavailableException.java
+++ Lab3/Netflix/src/bg/sofia/uni/fmi/mjt/netflix/exceptions/ContentUnavailableException.java
@@ -1,12 +1,12 @@
 package bg.sofia.uni.fmi.mjt.netflix.exceptions;
 
-//The Exception class is the superclass of checked exceptions
+// The Exception class is the superclass of checked exceptions
 public class ContentUnavailableException extends Exception {
     public ContentUnavailableException(String message) {
         super(message);
     }
 
-    //the reason (which is saved for later retrieval by the getCause() method). A null value is permitted, and indicates that the cause is nonexistent or unknown.
+    // Provides the reason (which is saved for later retrieval by the getCause() method). A null value is permitted, and indicates that the cause is nonexistent or unknown.
     public ContentUnavailableException(String message, Throwable reason) {
         super(message, reason);
     }
